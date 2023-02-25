feb01f27d0c81cb0db25a69900f3c05efc1f2ccd
Santyru <santiago0988@hotmail.com>
2023-02-24 23:00:29 -0300

    change dimensions

--- gamefiles/scenes/paddle.gd
+++ gamefiles/scenes/paddle.gd
@@ -1,17 +1,21 @@
 extends KinematicBody2D
 export(int) var speed = 400
+signal life_depleted
 
+var lifes = 3
 var direction = Vector2()
 var velocity = Vector2()
 
+func life_check():
+	if lifes == 0:
+		emit_signal("life_depleted")
+		
 func get_input ():
 	direction.x = 0
 	if Input.is_action_pressed("ui_right"):
 		direction.x = 1
-		print(1)
 	if Input.is_action_pressed("ui_left"):
 		direction.x = -1
-		print(-1)
 
 func _physics_process(_delta):
 	get_input()
@@ -20,3 +24,8 @@ func _physics_process(_delta):
 	
 func _ready():
 	pass # Replace with function body.
+
+
+func _on_Area2D_ball_exited(_body):
+	lifes -= 1
+	life_check()
