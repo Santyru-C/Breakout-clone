d37611dc81b8db961855c120bc3c55730451af9c
Santyru <santiago0988@hotmail.com>
2023-02-24 21:22:21 -0300

    intance bricks

--- gamefiles/Main.gd
+++ gamefiles/Main.gd
@@ -3,8 +3,25 @@ extends Node
 var ball_scene = load("res://scenes/ball.tscn")
 var ball = ball_scene.instance()
 
+func place_bricks():
+	var position_y = 25
+	for m in 8:
+		var position_x = 62
+		for n in 10:
+			var brick_scene = load("res://scenes/brick.tscn")
+			var brick = brick_scene.instance()
+			
+			if n != 0:
+				position_x += 100
+				
+			brick.set_position(Vector2(position_x, position_y))
+			add_child(brick)
+			
+		position_y += 23
+	
 func _ready():
 	add_child(ball)
+	place_bricks()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
