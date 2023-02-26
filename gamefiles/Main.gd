32a3d366b226777ba0c425401d39d11623bb43e4
Santyru <santiago0988@hotmail.com>
2023-02-25 23:02:33 -0300

    add new_game function

--- gamefiles/Main.gd
+++ gamefiles/Main.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var ball_scene = load("res://scenes/ball.tscn")
+var brick_name_container = []
 
 func update_life_display():
 	var format_text = "Lifes: %s"
@@ -19,21 +20,36 @@ func place_bricks():
 				position_x += 100
 				
 			brick.set_position(Vector2(position_x, position_y))
-			add_child(brick)
+			print(brick)
+			call_deferred("add_child", brick)
 			
 		position_y += 23
 
 func call_for_ball():
 	var ball = ball_scene.instance()
+	yield(get_tree().create_timer(0.25), "timeout")
 	call_deferred("add_child", ball)
-	
-func _ready():
-	call_for_ball()
+
+func reset_properties():
+	$paddle.lifes = 3
+	$score_display.text = "0"
+	get_tree().call_group("bricks", "queue_free")
+
+func new_game():
+	reset_properties()
 	place_bricks()
 	update_life_display()
+	call_for_ball()
+
+func game_over():
+	$paddle.speed = 0
+	
+	
+func _ready():
+	new_game()
 
 func _on_paddle_life_depleted():
-	pass # Replace with function body.
+	pass
 
 func _on_Area2D_body_exited(ball):
 	ball.queue_free()
